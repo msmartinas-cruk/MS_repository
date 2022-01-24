73229664fbd0cd5f8e43820c647c573e03d33057
Martina Slapkova <martina.slapkova@cancer.org.uk>
2022-01-24 17:31:42 +0000

    Updating script for CRC

--- SAIL/CAN06_07_CWT_DA.R
+++ SAIL/CAN06_07_CWT_DA.R
@@ -8,60 +8,42 @@
 
 library(tidyverse)
 library(statswalesr)
+library(janitor)
 library(ggplot2)
 
+setwd("~/MS_repository")
+
 # Pull data for 62 and 31 days from StatsWales - commented to avoid re-downloading. Accessed 21 January 2022.
-# data62 <- statswales_get_dataset("hlth0050") 
+data62 <- statswales_get_dataset("hlth0050") 
 data31 <- statswales_get_dataset("hlth0055")
 
-write.csv(data62, "data62.RData")
-write.csv(data31, "data31.RData")
-
-
-# Clean 31-day data TO DO
-
-select_data31 <- data31 %>% 
-  select (Data, LHBProvider_ItemName_ENG, Measure_ItemName_ENG, Date_Code, Date_ItemName_ENG, Site_ItemName_ENG) %>%
-  rename(value = Data, 
-         geography = LHBProvider_ItemName_ENG, 
-         measure = Measure_ItemName_ENG, 
-         date = Date_ItemName_ENG, 
-         site = Site_ItemName_ENG)
-
 # Clean 62-day data
-
-select_data62 <- data62 %>% 
-  select (Data, LHBProvider_ItemName_ENG, Measure_ItemName_ENG, Date_Code, Date_ItemName_ENG, Site_ItemName_ENG) %>%
+select_data <- data62 %>% 
+  filter(LHBProvider_ItemName_ENG == "Wales") %>%
+  select (Data, Measure_ItemName_ENG, Date_Code, Date_ItemName_ENG, Site_ItemName_ENG) %>%
   rename(value = Data, 
-         geography = LHBProvider_ItemName_ENG, 
          measure = Measure_ItemName_ENG, 
          date = Date_ItemName_ENG, 
          site = Site_ItemName_ENG)
 
-# Converting date into Date format
-select_data62$month <- substr(select_data62$Date_Code, start = 6, stop = 7)
-select_data62$year <- substr(select_data62$Date_Code, start = 1, stop = 4)
-select_data62$Date <- as.Date(paste("01", select_data62$month, select_data62$year, sep = "-"),"%d-%m-%Y")
+select_data$month <- substr(select_data$Date_Code, start = 6, stop = 7)
+select_data$year <- substr(select_data$Date_Code, start = 1, stop = 4)
+select_data$Date <- as.Date(paste("01", select_data$month, select_data$year, sep = "-"),"%d-%m-%Y")
 
-
-unique(select_data62$geography)
-unique(select_data62$measure)
-unique(select_data62$site)
-
-##### UCSR & 62-day wait #####
 
 ### Colorectal cancer
 
 ## 1. Preparing CRC data
 
-# Creating subset for CRC 
+# 1a. USCR & 62 days - Creating subset for CRC 
 crc_data <- select_data %>% 
   filter(site == "Lower Gastrointestinal", 
          Date >= "2019-01-01") %>%
-  select (date, Date, geography, measure, value) %>%
+  select (date, Date, measure, value) %>%
   group_by(measure, Date, date) %>%
   summarise(count = sum(value)) %>%
   spread(measure, count)
+
 
 crc_data$year <- substr(crc_data$Date, 1,4)
 crc_data$month <- substr(crc_data$Date, 6,7)
@@ -122,7 +104,7 @@ ggplot(crc_uscr, aes(month, prop_started_within_62, colour = year, group = year)
   scale_y_continuous(limit = c(0, 100)) 
 
 
-# 2B. lotting proportion of USCR referrals within 62 days with 2019 SDs
+# 2B. Plotting proportion of USCR referrals within 62 days with 2019 SDs
 
 ggplot(crc_uscr_2019SD, aes(month, prop_started_within_62, colour = year, group = year)) + 
   geom_point() + 
@@ -132,6 +114,28 @@ ggplot(crc_uscr_2019SD, aes(month, prop_started_within_62, colour = year, group 
   scale_y_continuous(limit = c(0, 100)) 
 
 ## 3. Data tables
+
+#TO DO - need to add 2020 monthly mean - just for April - November?
+
+uscr_2019_comparison <- d2020 %>%
+  select(Date:prop_started_within_62)
+
+uscr_2019_comparison$uscr_2019 <- mean(d2019$total_uscr_received) # calculating monthly mean for 2019 
+uscr_2019_comparison$uscr_sd_2019 <- sd(d2019$total_uscr_received) # calculating monthly SD for 2019 
+uscr_2019_comparison$uscr_prop_reduction <- 
+  ((uscr_2019_comparison$uscr_2019-uscr_2019_comparison$total_uscr_received)/uscr_2019_comparison$uscr_2019)*100 # calculating percent change 2019 vs 2020
+
+
+uscr_2019_comparison$within62_2019 <- mean(d2019$started_treatment_within_62) # calculating monthly mean for 2019 
+uscr_2019_comparison$within62_sd_2019 <- sd(d2019$started_treatment_within_62) # calculating monthly SD for 2019 
+uscr_2019_comparison$within62_prop_reduction <- 
+  ((uscr_2019_comparison$within62_2019-uscr_2019_comparison$started_treatment_within_62)/uscr_2019_comparison$within62_2019)*100 # calculating percent change 2019 vs 2020
+
+
+uscr_2019_comparison$total_starting_treatment_2019 <- mean(d2019$total_starting_treatment) # calculating monthly mean for 2019 
+uscr_2019_comparison$total_starting_treatment_sd_2019 <- sd(d2019$total_starting_treatment) # calculating monthly SD for 2019 
+uscr_2019_comparison$total_starting_treatment_prop_reduction <- 
+  ((uscr_2019_comparison$total_starting_treatment_2019-uscr_2019_comparison$total_starting_treatment)/uscr_2019_comparison$total_starting_treatment_2019)*100 # calculating percent change 2019 vs 2020
 
 
 
